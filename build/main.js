1bbe5f270583028bc16cd1f502e295153258e2ae
Vesko Vujovic <veskovujovic@yahoo.com>
2015-05-19 16:47:47 +0200

    making service for fethcing data
    fetch data service

--- build/main.js
+++ build/main.js
@@ -38,3 +38,22 @@ outsource.config(function($stateProvider){
     })
 });
 
+
+    //we are calling module
+	outsource.module('outsourceApp')
+    
+    //service for fetching json data
+    outsource.service('getService', function($http){
+
+        //fetch data from json file
+        /**
+         * @param path - this is the url of file
+         * @param part - this is the part of json array you want 
+         */
+       
+        this.fetchData = function(path, part){
+
+        };
+    });
+
+
---
+++ src/app/components/outsource/services/getdata.service.js
@@ -0,0 +1,19 @@
+
+    //we are calling module
+	outsource.module('outsourceApp')
+    
+    //service for fetching json data
+    outsource.service('getService', function($http){
+
+        //fetch data from json file
+        /**
+         * @param path - this is the url of file
+         * @param part - this is the part of json array you want 
+         */
+       
+        this.fetchData = function(path, part){
+
+        };
+    });
+
+
