6fd4f94c109055e7e6b38243abfffc109a9d7452
Vesko Vujovic <veskovujovic@yahoo.com>
2015-05-19 11:13:05 +0200

    solved
    i've solved problem with scripts

--- build/main.js
+++ build/main.js
@@ -2,7 +2,18 @@
  * This is the main config file in this app, here we define initialization and all other things
  */
 
-var outsource = angular.module('outsourceApp', ['ui.router']);
+var outsource = angular.module('outsourceApp', []);
+
+//module for angular routing
+var routes    = angular.module('route',['ui.router']);
+
+
+//config for routes
+routes.config(function($stateProvider){
+   $stateProvider
+       .state('index',{url:"/index", templateUrl: "views/main.html"});
+});
+
 (function(outsource){
     //we are calling module
 	outsource.module('outsourceApp')
