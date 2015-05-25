ce405147be99d63719fb40150fa72a326ad04849
Vesko Vujovic <veskovujovic@yahoo.com>
2015-05-25 09:28:18 +0200

    ui router index page working
    fixed small issue regarding index page

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -9,14 +9,27 @@
 var outsource = angular.module('outsourceApp', ['ui.router']);
 
 outsource.config(function($stateProvider, $urlRouterProvider){
-      $urlRouterProvider.otherwise('/');
-
+      $urlRouterProvider.otherwise('ne radi');
+      
+      /**
+        * These are route for app the first route is parent other routes are child routes
+        */
       $stateProvider
         .state('home', {
           url: '/home',
-          templateUrl: 'home.html',
+          templateUrl: '',
           controller: 'indexCtrl'          	          
-        });
+        })
+            .state('index', {
+            	parent: 'home',
+            	url: '/index',
+            	views:{
+            		inner:{
+            			templateUrl: 'src/app/components/views/homepage.html',
+            			controller: 'indexCtrl' 
+            		}
+            	}
+            })
 
 
 });
